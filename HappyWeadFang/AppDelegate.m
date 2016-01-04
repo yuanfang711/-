4892865ea5f3103be4e09b5a3cfc5cd69c7eee82
范芳芳 <1498012357@qq.com>
2016-01-04 17:11:41 +0800

    设置图片……

--- HappyWeadFang/AppDelegate.m
+++ HappyWeadFang/AppDelegate.m
@@ -12,7 +12,6 @@
 #import "MineViewController.h"
 
 
-
 @interface AppDelegate ()
 
 @end
@@ -24,19 +23,45 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     
+    //UITabBar
+    UITabBarController *tabbarC = [[UITabBarController alloc] init];
+    
+//主页
+    UIStoryboard *main = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    UINavigationController *mainNav = main.instantiateInitialViewController;
+    mainNav.tabBarItem.image = [UIImage imageNamed:@"ft_home_normal_ic.png"];
+    mainNav.tabBarItem.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+    UIImage *selectedImage = [UIImage imageNamed:@"ft_home_selected_ic.png"];
+    //设置选中图片按照图片的原始图片的状态显示
+    mainNav.tabBarItem.selectedImage = [selectedImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    
+//发现
+    UIStoryboard *find = [UIStoryboard storyboardWithName:@"Discover" bundle:nil];
+    UINavigationController *disNAV= find.instantiateInitialViewController;
+    disNAV.tabBarItem.image = [UIImage imageNamed:@"ft_found_normal_ic.png"];
+    disNAV.tabBarItem.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+    //设置选中图片按照图片的原始图片的状态显示
+    UIImage *selectedImage2 = [UIImage imageNamed:@"ft_found_selected_ic.png"];
+ 
+    mainNav.tabBarItem.selectedImage = [selectedImage2 imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     
     
+//我的
+    UIStoryboard *me = [UIStoryboard storyboardWithName:@"Mine" bundle:nil];
+    UINavigationController *meNAV = me.instantiateInitialViewController;
+    meNAV.tabBarItem.image = [UIImage imageNamed:@"ft_person_normal_ic.png"];
+    meNAV.tabBarItem.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+    //设置选中图片按照图片的原始图片的状态显示
+    UIImage *selectedImage3 = [UIImage imageNamed:@"ft_person_selected_ic.png"];
     
+    mainNav.tabBarItem.selectedImage = [selectedImage3 imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     
+    //添加到tabber的视图上
+    tabbarC.viewControllers = @[mainNav, disNAV, meNAV];
+    tabbarC.tabBar.backgroundColor = [UIColor whiteColor];
     
-    
-    
-    
-    MainViewController *mainVC= [[MainViewController alloc] init];
-    
-    self.window.rootViewController = mainVC;
-    
-    
+    //添加为根视图
+    self.window.rootViewController = tabbarC;
     
     
     self.window.backgroundColor = [UIColor whiteColor];
